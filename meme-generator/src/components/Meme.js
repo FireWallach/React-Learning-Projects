8a5a564b191d0a3338399d983db103d9d647e85a
Dylan Wallach <22645038+FireWallach@users.noreply.github.com>
2023-06-17 14:58:22 -0400

    Added State Changes to Meme

--- meme-generator/src/components/Meme.js
+++ meme-generator/src/components/Meme.js
@@ -1,12 +1,23 @@
 import React from "react";
 import memesData from "../memesData";
 
-const logMemeUrl = () => {
-  const randomNumber = Math.floor(Math.random() * (99 - 0 + 1));
-  console.log(memesData.data.memes[randomNumber].url);
-};
-
 const Meme = () => {
+  const [state, setState] = React.useState({
+    topText: "",
+    bottomText: "",
+    randomImage: "http://i.imgflip.com/1bij.jpg",
+    allMemeImages: memesData.data.memes,
+  });
+  const getMeme = () => {
+    const randomNumber = Math.floor(Math.random() * (99 - 0 + 1));
+    setState((prevState) => {
+      return {
+        ...prevState,
+        randomImage: state.allMemeImages[randomNumber].url,
+      };
+    });
+  };
+
   return (
     <main>
       <div className="form">
@@ -14,10 +25,13 @@ const Meme = () => {
           <input type="text" placeholder="Top Text" />
           <input type="text" placeholder="Bottom Text" />
         </div>
-        <button className="submitButton" onClick={logMemeUrl}>
+        <button className="submitButton" onClick={getMeme}>
           Get a new meme image 🖼
         </button>
       </div>
+      {state.randomImage && (
+        <img className="meme" src={state.randomImage} alt="Meme" />
+      )}
     </main>
   );
 };
